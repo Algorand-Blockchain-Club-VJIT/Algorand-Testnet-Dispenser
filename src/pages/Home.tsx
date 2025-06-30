6dc67ea9a6c4ad4d00f13c74e8aef49a9f8fd2a9
Vijay Guttula <vijayguttula001@gmail.com>
2025-07-01 01:32:58 +0530

    Added Algorand Icon in Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import algorandLogo from '../assets/react.svg' // Assume you have a logo SVG
+import algorandLogo from '/algorand.svg' // Assume you have a logo SVG
 
 const Home = () => {
   return (
