597f75dabbc57e19bc6eeebdd9078bfa51f272c0
Vijay Guttula <vijayguttula001@gmail.com>
2025-07-02 00:33:31 +0530

    Fixed pages issue in main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Dispense from './pages/Dispense'
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dispense" element={<Dispense />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>,
 )
